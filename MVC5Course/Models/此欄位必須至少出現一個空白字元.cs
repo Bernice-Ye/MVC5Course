f0c716a3372f3b7e7b99f7bc841b2286706d588c
Bernice Ye <bernice_ye@cy-tech.com.tw>
2016-03-06 17:23:20 +0800

    22 練習自訂 Models 的驗證屬性 ( 繼承 DataTypeAttribute )

---
+++ MVC5Course/Models/此欄位必須至少出現一個空白字元.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace MVC5Course.Models
+{
+    class 此欄位必須至少出現一個空白字元Attribute : DataTypeAttribute
+    {
+        public 此欄位必須至少出現一個空白字元Attribute():base(DataType.Text)
+        {
+
+        }
+        public override bool IsValid(object value)
+        {
+            var str = value.ToString();
+            return str.Contains(" ");
+
+        }
+    }
+}
