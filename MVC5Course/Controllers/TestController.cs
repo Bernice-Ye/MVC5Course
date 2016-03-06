abbc3e8aeaef52933b441be31489731ee870eb88
Bernice Ye <bernice_ye@cy-tech.com.tw>
2016-03-06 09:37:14 +0800

    創建ViewModel

--- MVC5Course/Controllers/TestController.cs
+++ MVC5Course/Controllers/TestController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC5Course.Models;
+
 
 namespace MVC5Course.Controllers
 {
@@ -10,6 +12,17 @@ namespace MVC5Course.Controllers
     {
         // GET: Test
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MemberProfile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MemberProfile(MemberViewModel Data)
         {
             return View();
         }
---
+++ MVC5Course/Models/MemberViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class MemberViewModel
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
